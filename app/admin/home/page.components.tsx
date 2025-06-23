d9165b900d6f92203f831bc5f26e1f86f9f957a2
Yerassyl <zhasulanov.erasyl@mail.ru>
2025-06-23 20:45:43 +0500

    Edit files to create production build

---
+++ app/admin/home/page.components.tsx
@@ -0,0 +1,21 @@
+import GenerationInput from "@/features/admin/components/GenerationInput";
+import GenerationParameters from "@/features/admin/components/GenerationParameters";
+
+export function Main() {
+  return (
+    <div
+      className={`w-full flex-1 flex flex-col items-center justify-center gap-[60px]`}
+    >
+      <div className="w-full flex flex-col items-center justify-center">
+        <p className="text-bold md:text-[70px] text-[48px]">Сгенерировать</p>
+        <p className="text-bold md:text-[32px] text-[20px]">
+          Что бы вы хотели создать сегодня?
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 p-4 md:w-[750px] w-full">
+        <GenerationParameters />
+        <GenerationInput />
+      </div>
+    </div>
+  );
+}
--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -1,33 +1,13 @@
 "use client";
 
-import GenerationInput from "@/features/admin/components/GenerationInput";
-import GenerationParameters from "@/features/admin/components/GenerationParameters";
 import { GenerationProvider } from "@/features/admin/context/GenerationContext";
 import React from "react";
+import { Main } from "./page.components";
 
-export default function Wrapper() {
+export default function Page() {
   return (
     <GenerationProvider>
-      <Page />
+      <Main />
     </GenerationProvider>
-  );
-}
-
-export function Page() {
-  return (
-    <div
-      className={`w-full flex-1 flex flex-col items-center justify-center gap-[60px]`}
-    >
-      <div className="w-full flex flex-col items-center justify-center">
-        <p className="text-bold md:text-[70px] text-[48px]">Сгенерировать</p>
-        <p className="text-bold md:text-[32px] text-[20px]">
-          Что бы вы хотели создать сегодня?
-        </p>
-      </div>
-      <div className="flex flex-col gap-2 p-4 md:w-[750px] w-full">
-        <GenerationParameters />
-        <GenerationInput />
-      </div>
-    </div>
   );
 }
